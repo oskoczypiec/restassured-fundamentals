c5df70d60b84de0fd0391e3b9d982f2b6bb9670b
Aleksandra Skoczypiec <askoczypiec@objectivity.co.uk>
2022-02-17 18:47:07 +0100

    Initial commit

---
+++ src/test/java/com/psrestassured/BasicTest.java
@@ -0,0 +1,13 @@
+package com.psrestassured;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class BasicTest {
+    @Test
+    public void someTest(){
+        RestAssured.get("https://api.github.com/")
+                .then()
+                .statusCode(200);
+    }
+}
